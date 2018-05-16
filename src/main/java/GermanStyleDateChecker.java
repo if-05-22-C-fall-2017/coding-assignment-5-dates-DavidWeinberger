a25436065d98831ec0be09317dbe3aeed8a15e03
DavidWeinberger <31950991+DavidWeinberger@users.noreply.github.com>
2018-05-16 15:59:30 +0200

    Add files via upload

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -2,7 +2,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
-    public static Matcher getMatcher(String dateString) {
-        return null;
+    static Matcher getMatcher(String input) {
+        Pattern p2 = Pattern.compile("(^([1-2][0-9])|^(0{0,1}[1-9])|^(3(0|1)))(\\.|-| )((1[0-2])|(0{0,1}[1-9]))(\\.|-| )([1-2](0|9)){0,1}[0-9][0-9]$");
+        Matcher m2 = p2.matcher(input);
+        return m2;
     }
 }
